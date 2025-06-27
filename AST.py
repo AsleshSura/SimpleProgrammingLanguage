ca4fc8f70e9e1c35dcb33408ca784775b0f774c8
SOLID <solidbiker3000@gmail.com>
2025-06-27 15:43:53 -0500

    Added all AST

--- AST.py
+++ AST.py
@@ -1,11 +1,13 @@
-
-
 class ASTNode:
     pass
 
 class Number(ASTNode):
     def __init__(self, value):
-        self.value = int(value)
+        self.value = float(value)
+
+class String(ASTNode):
+    def __init__(self, value):
+        self.value = value
 
 class Variable(ASTNode):
     def __init__(self, name):
@@ -17,6 +19,11 @@ class BinOp(ASTNode):
         self.op = op
         self.right = right
 
+class UnaryOp(ASTNode):
+    def __init__(self, op, operand):
+        self.op = op
+        self.operand = operand
+
 class Assign(ASTNode):
     def __init__(self, name, value):
         self.name = name
@@ -25,6 +32,17 @@ class Assign(ASTNode):
 class Print(ASTNode):
     def __init__(self, expr):
         self.expr = expr
+
+class If(ASTNode):
+    def __init__(self, condition, then_branch, else_branch=None):
+        self.condition = condition
+        self.then_branch = then_branch
+        self.else_branch = else_branch
+
+class While(ASTNode):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
 
 class Program(ASTNode):
     def __init__(self, statements):
